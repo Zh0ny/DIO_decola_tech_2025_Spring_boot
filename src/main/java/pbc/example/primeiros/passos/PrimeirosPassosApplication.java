3083e728a7e6589536134e6c63fe6822b27980f1
Zh0ny <caio.philipe.queiroz@gmail.com>
2025-03-06 17:58:18 -0300

    Add CommandLineRunner bean to PrimeirosPassosApplication
    This commit is part of a bootcamp project on Spring Boot, following the "Scopes - Singleton ou Prototype" video. It serves as a progress checkpoint, adding a CommandLineRunner bean to PrimeirosPassosApplication to send confirmation and welcome messages using the SistemaMensagem component.

--- src/main/java/pbc/example/primeiros/passos/PrimeirosPassosApplication.java
+++ src/main/java/pbc/example/primeiros/passos/PrimeirosPassosApplication.java
@@ -12,11 +12,10 @@ public class PrimeirosPassosApplication {
 		SpringApplication.run(PrimeirosPassosApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner run(ConversorJson conversorJson) {
+	public CommandLineRunner run(SistemaMensagem sistema) {
 		return args -> {
-			String json = "{\"cep\":\"01001-000\",\"logradouro\":\"Praça da Sé\",\"localidade\":\"São Paulo\"}";
-			ViaCepResponse response = conversorJson.converter(json);
-			System.out.println("Dados do CEP: " + response);
+			sistema.enviarConfirmacao();
+			sistema.enviarMensagemBoasVindas();
 		};
 	}
 }
